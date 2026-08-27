b54384eda47d94c55257416bf75e8c5ea79b877e
Hamere Getachew <hameregetachew.151@gmail.com>
2026-08-27 23:05:04 +0300

    Update RC4_encryptor.cpp

--- RC4_encryptor.cpp
+++ RC4_encryptor.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+int main()
+{ 
+  string key = "INSA2026";
+  ifstream input("file.txt" , ios::binary);
+  if (!input)
+  {
+    cout << "can not open file.txt"  << endl;
+  return1;
+  }
+  return 0;
+}
